ddfa86de737e8290431d7cc4ffcffbc942f62d10
adrianacoliiin <adriana.colinmunoz@gmail.com>
2025-06-14 14:07:20 -0600

    estructura front

---
+++ frontend/src/navigation/TabNavigator.js
@@ -0,0 +1,4 @@
+import ChatbotScreen from "../screens/Chatbot";
+import MapsScreen from "../screens/Maps";
+
+const TabNavigator = 
---
+++ frontend/src/screens/Chatbot.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+export default function ChatbotScreen() {
+    return (
+        <div>Pantalla de CHatbot</div>
+    )
+}
