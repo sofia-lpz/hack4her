c5f08d66d55deddd8141e54e072babca8862ee68
sofia-lpz <a01028251@tec.mx>
2025-06-14 19:41:43 -0600

    added example of supabase

--- frontend/src/api/dataProvider.js
+++ frontend/src/api/dataProvider.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 const API_BASE_URL = 'http://10.22.138.54:8080/api';
+const API_BASE_URL_SUPABASE = 'https://kjsganrpfhivqpruovrg.supabase.co/rest/v1/';
+const API_KEY = 'your_api_key_here'; 
+
 
 export const fetchChat = async (prompt) => {
   try {
@@ -10,3 +13,30 @@ export const fetchChat = async (prompt) => {
     throw error;
   }
 }
+
+export const login = async (username, password) => {
+  try {
+    const response = await axios.post(`${API_BASE_URL}/login`, { username, password });
+    return response.data;
+  } catch (error) {
+    console.error('Error during login:', error);
+    throw error;
+  }
+}
+
+export const fetchUsers = async () => {
+  try {
+    const response = await axios.get(`${API_BASE_URL_SUPABASE}users`, {
+      headers: supabaseHeaders
+    });
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching users:', error);
+    throw error;
+  }
+}
+
+const supabaseHeaders = {
+  'Content-Type': 'application/json',
+  'apikey': API_KEY
+  };
